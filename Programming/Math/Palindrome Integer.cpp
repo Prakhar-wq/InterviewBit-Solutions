975cc93d1954799460bae1e720fa8d984c41ba6b
rahulotwani <rahulotwani7@gmail.com>
2020-10-18 16:25:40 +0530

    added two solutions cpp

---
+++ Programming/Math/Palindrome Integer.cpp
@@ -0,0 +1,12 @@
+int reverse(int A) {
+    int rev=0;
+    while(A>0) {
+        rev=rev*10+A%10;
+        A=A/10;
+    }
+return rev;
+}
+
+int Solution::isPalindrome(int A) {
+   return A==reverse(A);
+}
